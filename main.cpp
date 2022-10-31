e20ff2c5779b0e425e50b9720d1acbdbdded0dde
Jim Shi <shiyilin2008@gmail.com>
2022-10-31 00:45:15 -0700

    Add main function and command line parser for `findSeed`

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+#include "seed_helper.h"
+
+
+void find(const std::string_view& brandName, const std::vector<std::string_view>& previousRolls) {
+//    std::cout << brandName << std::endl;
+//    for (const auto& r: previousRolls) {
+//        std::cout << r << std::endl;
+//    }
+
+    auto seedHelper = SeedHelper(brandName);
+    const auto results = seedHelper.findSeed(previousRolls);
+    if (!results.empty()) {
+        for (const auto& result: results) {
+            std::cout << std::hex << "0x" << result << "\n";
+        }
+    }
+}
+
+
+void predict() {
+    // TODO:
+}
+
+
+namespace ErrorCode {
+    constexpr int NO_COMMAND = 1;
+    constexpr int INVALID_COMMAND = 2;
+    constexpr int NOT_ENOUGH_ARGUMENTS = 3;
+    constexpr int NOT_IMPLEMENTED = 128;
+}
+
+
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
+        std::cout << "No command given." << std::endl;
+        return ErrorCode::NO_COMMAND;
+    }
+
+    const std::string_view command {argv[1]};
+    if (command == "find") {
+        if (argc < 4) {
+            std::cout << "Not enough arguments." << std::endl;
+            return ErrorCode::NOT_ENOUGH_ARGUMENTS;
+        }
+
+        const std::string_view brandName {argv[2]};
+
+        std::vector<std::string_view> previousRolls {};
+        for (int i = 3; i < argc; i += 1) {
+            previousRolls.emplace_back(argv[i]);
+        }
+
+        find(brandName, previousRolls);
+    } else if (command == "predict") {
+        std::cout << "Not implemented." << std::endl;
+        return ErrorCode::NOT_IMPLEMENTED;
+    } else {
+        std::cout << "Invalid command `" << command << "`." << std::endl;
+        return ErrorCode::INVALID_COMMAND;
+    }
+
+    return 0;
+}
